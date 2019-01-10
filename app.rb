222974c9d76c7ae9912366f703d435b6699fc7df
Certil Remy <certil@noukod.com>
2019-01-10 17:58:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,6 @@ class App < Sinatra::Base
 
   # Add your post route and action below
   post '/food' do
-    erb :food
-  end
-
+  "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+end
 end
